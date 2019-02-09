c2cf8d208d9847c4560cfb221c22d06b65067e1f
Cooper Pellaton <cooper@cooperpellaton.com>
2019-02-09 01:20:08 -0500

    [update] Add more files into the setup script.
    Former-commit-id: cad7951db1abe7ef92f454d6c1a335051270c55d
    Former-commit-id: df5d839db1967973cf32318862fbe4bdefdc688c
    Former-commit-id: 9637ea7fbf5ec83bf50f7fa04e6c013559d26b4a [formerly d2d6825a2711ac68da7050bfe2c907572c66ecdb] [formerly e62d5965d93dab62a2cee408ab8c5f2e631cfb21 [formerly 50f592af1b21d895be94b0ee0fe8a3805c38d35d]] [formerly 83afca4fad4e5077dede5fba36310c6b1af7412e [formerly 6334c1d4b5ddc7159fd583b13258863a4458fe74] [formerly e73352a17138f57d792b495287f24933eea0cd15 [formerly 6b21cfd772c210da6165ca3f925d9a9b7b30b838]]]
    Former-commit-id: dfdea121bc4dcc29b677534851954057d6470ed3 [formerly ec4b4278b149f94e9fd6a7e75765775ffd223a6a] [formerly 0a6b12baf6eb4fa04b4e3d9e6b8d21b6fcdb9701 [formerly 5e5781779ce1905e745cba3bd2601583fc7d8d77]]
    Former-commit-id: fe6af47d7e800891e7456d4ae3abfff74cfd203e [formerly 607ca38643b9c2e48ae4cdcc9ae321766d02028b]
    Former-commit-id: 88b259f48348bd9321227ca98335aae831b59358

--- setup.sh
+++ setup.sh
@@ -4,10 +4,17 @@
 # This is a setup script for my dotfiles. This *must* be run with `sudo`!
 
 cd ~
+
+# Install all the files programs and themes, etc. that we want.
 sudo apt install git stow tmux neovim ranger fonts-powerline -y
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 git clone https://github.com/jimeh/tmux-themepack.git ~/.tmux-themepack
 git clone https://github.com/cooperpellaton/dotfiles.git
+wget https://github.com/sharkdp/bat/releases/download/v0.10.0/bat_0.10.0_amd64.deb
+sudo dpkg -i bat_0.10.0_amd64.deb
+rm bat_0.10.0_amd64.deb
+
+# Now unpack the configuration files for our programs.
 cd dotfiles
 stow tmux
 stow bash
@@ -16,3 +23,5 @@ stow nvim
 stow vim
 stow themes
 vim +PluginInstall +qall
+
+echo "All files and programs have been installed!\m"
