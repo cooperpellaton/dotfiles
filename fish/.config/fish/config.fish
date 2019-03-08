7ab32b07c882f735d2cd7e245cc6788897cd4bf5
Cooper Pellaton <cooper@cooperpellaton.com>
2019-03-08 18:41:07 -0500

    [add] cleanup alias
    Former-commit-id: 604ac14a4a1e5251bd115d216be7b4971500ce1d
    Former-commit-id: 9fd96661fb00b0828b299b601f495e590e67bf44
    Former-commit-id: c034a096f97e89761f716dc1086c11f4688a08df [formerly 91279e38dd15a676a3b9eea1a8a520dcc7c73b6f] [formerly 20034101a2cb90b46bd017caeffd7a2112751d93 [formerly e85f7835a73bcdcf8d349a1a928452a6af8fb5a1]] [formerly c640f2adfc2c59c6cc123a3e9f22e32f765e8333 [formerly 966d3acd6001eeebc3250f1da27ec4c695c927b7] [formerly 4d8676d20e1e6fb52f3c60753576d93f88ae4866 [formerly 5c1e10aa8f3a9ed533f1cb6aa89ed092e422fa26]]]
    Former-commit-id: 0fbad95f8f479cd29f1ecc93fb5cbd6432476efa [formerly 447de60c01613da6de6ac614c60e430691f6f4ba] [formerly 59efcadd13cb0ed8c281592b2e61e919105e207b [formerly fbd7354fe247a3dff0fad489be16e563e5172399]]
    Former-commit-id: 38ff24b49601882210ecb322f2447940e992ca9a [formerly 943422eeea817e4b8c14f6f37e3890432c400760]
    Former-commit-id: 390cc29a3bbe455b405f05c2a1ec711fb2719d90

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -13,4 +13,5 @@ setenv EDITOR nvim
 
 # system aliases
 alias upgrade="sudo apt update; and sudo apt upgrade -y"
+alias cleanup="sudo apt autoremove -y; and sudo apt autoclean"
 thefuck --alias | source
