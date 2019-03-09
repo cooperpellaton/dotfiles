3b5a59cdf2bbec6ac6774840e89a29c6a524a57c
Cooper Pellaton <cooper@cooperpellaton.com>
2019-03-09 09:00:37 -0500

    [fix] New fish prompt and TMUX theme.
    Former-commit-id: ed07a62ab9fbb28df19596b8a09c9d98eeec6a95
    Former-commit-id: 634f03929796c6200dbdf4fb5cca446879dd71c8
    Former-commit-id: e01195fe8dc3ca55e4904e06d81cae57d80d3440 [formerly 0730cb37f50aee3eee2fd7fc5695343ca317f92d] [formerly 8340c4ed555f03b9463a87809ef8c2028d277093 [formerly 294409ab1a8e3bb78e7cb510d0a8f8c75d77251e]] [formerly 74a72cebaeaee22b800f839b3f1560cabe26814e [formerly 496353616e5989b2a4ed744f984f7ed9ba14aadd] [formerly df03c495998932cc0221f3b20a20f70b75d2278b [formerly da660b36fe914434fcc0cddd51c8c6a9880d6895]]]
    Former-commit-id: dcd4a39dfad69bc85976ef15fc85241377df9d82 [formerly f0f1fb164d8550d784fa8483ec257512813631a5] [formerly 3504a9eb8ed78fedf5e0632bd5404f830f68bf17 [formerly 69f5a9b2b37ef8f2200349f735e9de160cec1ec2]]
    Former-commit-id: a6f4bfb88ab665687e5c453b407a3b8a0cfd733a [formerly 6529b3be2efd789b8403ed9efeb43c5737ca77d1]
    Former-commit-id: 7c85ccc6a9694f15ee2964e55696fa0c8477d113

--- setup.fish
+++ setup.fish
@@ -5,6 +5,6 @@
 curl https://git.io/fisher --create-dirs -sLo ~/.config/fish/functions/fisher.fish
 
 # Install fish plugins
-fisher add oh-my-fish/theme-bobthefish
-echo "All files and programs have been installed!\n";
+fisher add oh-my-fish/theme-batman
+echo "All files and programs have been installed!";
 fish
