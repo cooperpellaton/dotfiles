c4b40d83380d8c2fd9bdae4070208646b2d1e832
Cooper Pellaton <cooper@cooperpellaton.com>
2021-02-18 08:59:06 +0000

    Update fish config.
    Former-commit-id: dd32fc2ecd09e801809eec02d832fc377286bd0e
    Former-commit-id: 34d2e3be1882c2ea4e52c8fa2e48c32ae67c929a
    Former-commit-id: 03049d77f88d58e9805575804585d2a7d2ed27b4 [formerly 8db6832af7bae0b548faadff3159aa6c7c91d778] [formerly e7f2fd5003a721cde47370fbd361c189ae8170d6 [formerly d3c914b1f920c7c37040936b7f480ab5e941502d]] [formerly 9f49546eb726e0c5299b2d79d4751db10c872a03 [formerly f87290887c513fb9d080315d1669b68340b10966] [formerly f99d452053b3fa0ab44eb821beec95ac9f5c11a2 [formerly 9253dfdd96449c438e0a6f94c0a5bb5620a6f859]]]
    Former-commit-id: d0d261292a96e2e92c6498481792048483ce0861 [formerly 80f2257661ad35f2079b92041c080307dd518cee] [formerly 6e7a0263f98b18f327168436e784710662b22bcd [formerly af9a64c9d5884da042f15475c262351033877a97]]
    Former-commit-id: 1553090cff041aa3e2e9689c035b358dadbacdec [formerly 2da8925ffff228ea91e0c52d14b04d9afb0c8cf0]
    Former-commit-id: 8229a148d5f3d83610e353382aad5cc9f8d14870

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -3,11 +3,6 @@
 # start_tmux
 # exec tmux attach
 
-# alias vim to neovim because we're ~~modern~~
-alias vim="nvim"
-alias vi="nvim"
-setenv EDITOR nvim
-
 # Environment
 export BAT_CONFIG_PATH="~/.config/bat/config"
 
@@ -15,39 +10,15 @@ export BAT_CONFIG_PATH="~/.config/bat/config"
 fish_vi_key_bindings
 
 # ALIASES
-alias uc="brew upgrade; brew cleanup"
 alias dockerclean="docker system prune -a -f; docker volume prune -f; docker image prune -a -f"
 alias k=kubectl
 alias tf=terraform
 alias g=gcloud
-thefuck --alias | source
-
-## GIT
-# Remove all traces of git from a folder
-alias degit="find . -name '.git' -exec rm -rf {} \;"
-# add and remove new/deleted files from git index automatically
-alias gitar="git ls-files -d -m -o -z --exclude-standard | xargs -0 git update-index --add --remove"
-# Git common
-alias get="git clone"
-alias gd="git diff"
-alias gs="git status -bs"
-
-# Get current external IP
-alias ip="curl icanhazip.com"
-
-# copy the working directory path
-alias cpwd='pwd|tr -d "\n"|pbcopy'
-
-# Source the Conda fish config.
-source /usr/local/anaconda3/etc/fish/conf.d/conda.fish
-
-# The next line enables shell command completion for gcloud.
-source /usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.fish.inc
-bass source /usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/completion.bash.inc
+alias ip="curl icanhazip.com" # Get current external IP
+# alias vim to neovim because we're ~~modern~~
+alias vim="nvim"
+alias vi="nvim"
+set -x EDITOR nvim
 
 # Use RipGrep in FZF
 export FZF_DEFAULT_COMMAND='rg --files --no-ignore-vcs --hidden'
-
-function fish_greeting
-  fortune -a
-end
