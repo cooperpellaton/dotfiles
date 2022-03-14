49a1f9d6b61de796befca67aece10bfcb276b60d
Cooper Pellaton <cooper@memorahealth.com>
2022-03-14 11:20:17 -0700

    Use abbr rather than alias.

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -5,6 +5,8 @@
 
 # Environment
 set BAT_CONFIG_PATH ~/.config/bat/config
+# Use RipGrep in FZF
+set FZF_DEFAULT_COMMAND 'rg --files --no-ignore-vcs --hidden'
 
 # note: this erases the default fish_mode_prompt, which adds a vi mode
 # indicator to the left of your prompt
@@ -14,18 +16,16 @@ function fish_mode_prompt; end
 fish_vi_key_bindings
 
 # ALIASES
-alias dockerclean="docker system prune -a -f; docker volume prune -f; docker image prune -a -f"
-alias k=kubectl
-alias tf=terraform
-alias g=gcloud
-alias ip="curl icanhazip.com" # Get current external IP
+# abbr -a -g dockerclean docker system prune -a -f; docker volume prune -f; docker image prune -a -f
+abbr -a -g k kubectl
+abbr -a -g tf terraform
+abbr -a -g g gcloud
+abbr -a -g ip curl icanhazip.com # Get current external IP
 # alias vim to nvim because we're ~~modern~~
 alias vim="nvim"
 alias vi="nvim"
 alias rm="rm -i"
 set -x EDITOR nvim 
 
-# Use RipGrep in FZF
-set FZF_DEFAULT_COMMAND 'rg --files --no-ignore-vcs --hidden'
-
 zoxide init fish | source
+kubectl completion fish | source
