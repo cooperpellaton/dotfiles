48add228bc172eb92416f5baca24dd6234be7b1b
Cooper Pellaton <c@cepp.ch>
2022-08-28 22:37:47 -0700

    Bum down fish config.

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -3,9 +3,6 @@
 # start_tmux
 # exec tmux attach
 
-# Environment
-set BAT_CONFIG_PATH ~/.config/bat/config
-
 # note: this erases the default fish_mode_prompt, which adds a vi mode
 # indicator to the left of your prompt
 function fish_mode_prompt; end
@@ -15,11 +12,9 @@ fish_vi_key_bindings
 
 # ALIASES
 # abbr -a -g dockerclean docker system prune -a -f; docker volume prune -f; docker image prune -a -f
-abbr -a -g k kubectl
-abbr -a -g tf terraform
-abbr -a -g g gcloud
 abbr -a -g ip curl icanhazip.com # Get current external IP
 alias rm="rm -i"
+# relies on https://briansunter.com/blog/emacs-daemon-macos/
+alias emacs="emacsclient -c"
 
 zoxide init fish | source
-# kubectl completion fish | source
